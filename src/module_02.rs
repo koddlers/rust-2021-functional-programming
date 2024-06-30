6ff36c66f86ebd391fb3a012183a5981592f38f2
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-06-30 21:10:24 +0600

    Add Module 02 - Introducing Functional Programming > Lesson 02 - What Is Functional Programming

--- src/module_02.rs
+++ src/module_02.rs
@@ -1,5 +1,17 @@
 pub mod introducing_functional_programming {
     pub fn what_is_functional_programming() {
-        println!("Hello, functional world!");
+        // Imperative
+        let numbers = vec![1, 2, 3, 4, 5];
+        let mut sum = 0;
+
+        for number in numbers {
+            sum += number;
+        }
+        println!("Sum: {}", sum);
+
+        // Functional
+        let numbers = vec![1, 2, 3, 4, 5];
+        let sum = numbers.iter().fold(0, |acc, x| acc + x);
+        println!("Sum: {}\n", sum);
     }
 }
