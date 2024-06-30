722abd415aa74b0f84385f1836533fde26aac059
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-06-30 20:43:05 +0600

    init("Functional Programming in Rust 2021")

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod module_02;
+
+use module_02::introducing_functional_programming as intro_fp;
+
+fn main() {
+    // Module 02 - Introducing Functional Programming
+    intro_fp::what_is_functional_programming();
+}
---
+++ src/module_02.rs
@@ -0,0 +1,5 @@
+pub mod introducing_functional_programming {
+    pub fn what_is_functional_programming() {
+        println!("Hello, functional world!");
+    }
+}
